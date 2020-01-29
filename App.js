a67a2808f7719c72011eccb72b62855bfcfd9081
sgharai <sofiagharaibeh@gmail.com>
2020-01-29 13:40:33 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+function alertFunction() {
+    alert("if day has to become night this is a beautiful way")
+}
+
+let mainFooter = document.getElementById("mainFooter")
+
+mainFooter.addEventListener("mouseover", function(event){
+    event.target.style.color = "purple";
+});
