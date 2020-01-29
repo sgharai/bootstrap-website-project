1cb7f8e8648280148d61fa531e39cee52e133250
sgharai <sofiagharaibeh@gmail.com>
2020-01-29 14:28:04 -0500

    removed unused eventListener

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
 function alertFunction() {
     alert("if day has to become night this is a beautiful way")
 }
-
-let mainFooter = document.getElementById("mainFooter")
-
-mainFooter.addEventListener("mouseover", function(event){
-    event.target.style.color = "purple";
-});
